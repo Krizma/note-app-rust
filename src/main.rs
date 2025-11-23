485324b618fdcd72150057aad2dd8541f7454587
Reid <antikrizma@gmail.com>
2025-11-23 17:39:41 -0600

    Initialize Rust note app with basic structure
    Add initial project files for a Rust-based note app, including .gitignore updates, Cargo.toml with rusqlite dependency, and a main.rs with function planning comments.

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/*Functions Needed
+
+create_folder()
+check_if_folder()
+check_note_db()
+callback()
+create_db_table_if_not_exists()
+check_if_note_already_exists()
+get_case_switch_command_number() --Improve this probs
+create_note()
+read_note()
+trim()
+delete_note()
+modify_note()
+create_config() --Not in C version
+read_config() --Not in C version
+
+
+*/
