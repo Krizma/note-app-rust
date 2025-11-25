4fdc411726ca3adfbbdbfe034f3f796873b0803e
Reid <antikrizma@gmail.com>
2025-11-25 14:26:41 -0600

    Added more main functionality and checking and creating folders
    as title says

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,10 @@ create_config() --Not in C version
 read_config() --Not in C version
 */
 
-use core::{error, panic};
-use std::{env, io::Error, process::exit};
+use core::{panic};
+use std::{env, fs::create_dir, path::PathBuf};
+use dirs::{home_dir};
+
 
 struct Note {
     title: String,
@@ -27,7 +29,9 @@ struct Note {
 }
 
 fn main() {
-    println!("RustNote App Version: 0.0.1");
+    let max_length:u32 = u32::MAX;
+
+    println!("RustNote App Version: 0.0.1\n");
     let args: Vec<String> = env::args().collect();
     //dbg!(args);
     //if args.len() != 3 {
@@ -35,26 +39,34 @@ fn main() {
     //}
     match args.len()  { 3 => (),_=>panic!("You did not specify the appropriate amount of args\n") }
     if args[1].len() <= 0 || args[2].len() <= 0  {
-        panic!("Unexpected NULL Argument");
+        panic!("Unexpected NULL Argument\n");
 
     }
 
     let command = args[2].to_string();
     let note_number: u32= args[1].parse().unwrap();
 
-    println!("{command},{note_number}"); 
+    println!("DEBUG: Launch Parameters: {},{}", note_number,command); 
+    
+    if note_number >= max_length {
+        panic!("Your input exceeds the size of the database.\n")
+    }
 
-        
-
+    let my_dir:PathBuf = match home_dir() {
+        Some(my_dir) => my_dir,
+        None => panic!("Could not find users home directory. You will have to set up a config and point to where you would like to store data for the program\n")
+    };
+    
+    println!("DEBUG: Home: {}",my_dir.to_string_lossy());
+    check_if_folder_exists_or_create_folder(my_dir);
 }
 
-fn create_folder() {
-
+fn check_if_folder_exists_or_create_folder(my_dir:PathBuf) -> std::io::Result<()> {
+    create_dir(my_dir.to_string_lossy()+"/note")?;
+    println!("DEBUG: Dir concact outcome: {}/note",my_dir.to_string_lossy());
+    Ok(());
 }
 
-fn check_if_folder_exists() {
-
-}
 
 fn check_if_note_db_exists() {
 
