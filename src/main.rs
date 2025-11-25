8b189c3c17892001f178c6236beb2ff8f340fad8
Reid <6732108+Krizma@users.noreply.github.com>
2025-11-25 05:47:20 -0600

    Remove Jenkinsfile and start RustNote app structure
    Deleted the Jenkinsfile and began implementing the RustNote application in main.rs. Added a Note struct, argument parsing, and function stubs for note and config management to lay the groundwork for further development.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-fn main() {
-    println!("Hello, world!");
-    println!("This is the dev branch")
-}
-
-/*Functions Needed
+/*Things Needed
+Message Struct
 
 create_folder()
 check_if_folder()
@@ -19,6 +15,95 @@ delete_note()
 modify_note()
 create_config() --Not in C version
 read_config() --Not in C version
-
-
 */
+
+use core::{error, panic};
+use std::{env, io::Error, process::exit};
+
+struct Note {
+    title: String,
+    body: String,
+    time: String,
+}
+
+fn main() {
+    println!("RustNote App Version: 0.0.1");
+    let args: Vec<String> = env::args().collect();
+    //dbg!(args);
+    //if args.len() != 3 {
+    //   
+    //}
+    match args.len()  { 3 => (),_=>panic!("You did not specify the appropriate amount of args\n") }
+    if args[1].len() <= 0 || args[2].len() <= 0  {
+        panic!("Unexpected NULL Argument");
+
+    }
+
+    let command = args[2].to_string();
+    let note_number: u32= args[1].parse().unwrap();
+
+    println!("{command},{note_number}"); 
+
+        
+
+}
+
+fn create_folder() {
+
+}
+
+fn check_if_folder_exists() {
+
+}
+
+fn check_if_note_db_exists() {
+
+}
+
+fn create_db_table_if_not_exists() {
+
+}
+fn check_if_note_already_exists() {
+
+}
+
+fn get_case_switch_command_number() {
+
+}
+
+fn create_note() {
+
+}
+
+fn read_note() {
+
+}
+fn delete_note() {
+
+}
+
+fn modify_note() {
+
+}
+
+fn modify_config() {
+
+}
+
+fn restore_default_config() {
+
+}
+
+fn read_config() {
+
+}
+
+fn show_written_notes_num_and_title() {
+
+}
+
+
+
+
+
+
