cb72a90eae28be893b16b40cfb25ccc5516c80c5
Reid <6732108+Krizma@users.noreply.github.com>
2025-11-29 04:33:53 -0600

    some eod

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,9 @@ read_config() --Not in C version
 
 use core::panic;
 use dirs::home_dir;
-use rusqlite::{Connection, Error, ErrorCode};
+use rusqlite::{Connection, MAIN_DB, Result, TEMP_DB, ffi::SQLITE_ATTACH};
 use std::{
-    env::{self, consts::OS},
-    fs::create_dir,
-    io::ErrorKind,
-    path::{Path, PathBuf},
+    env::{self}, fs::create_dir, io::ErrorKind, path::PathBuf
 };
 
 struct Note {
@@ -68,8 +65,9 @@ fn main() {
     };
 
     println!("DEBUG: Home: {}", my_dir.to_string_lossy());
-    let db_path = check_if_folder_and_db_exists_or_create(my_dir);
-    create_db_table_if_not_exists(db_path);
+    let db_path: String = check_if_folder_and_db_exists_or_create(my_dir);
+    create_db_table_if_not_exists(db_path.clone());
+    check_if_note_already_exists(db_path,note_number);
 }
 
 fn check_if_folder_and_db_exists_or_create(my_dir: PathBuf) -> String {
@@ -116,15 +114,57 @@ fn check_if_folder_and_db_exists_or_create(my_dir: PathBuf) -> String {
     }
     // println!("{}", db_file.to_string_lossy().into_owned());
 }
-
-fn create_db_table_if_not_exists(db_path: String) -> Result<(), Error> {
-    let db_connect = Connection::open(db_path)?;
+// fn open_db(db_path: String) -> Connection{
+//     let db_connect = Connection::open(db_path);
+//     return db_connectction;
+// }
+fn create_db_table_if_not_exists(db_path: String) -> Result<()> {
     let sql: &str = "CREATE TABLE IF NOT EXISTS notes(ID INT PRIMARY KEY NOT NULL, TIME INT NOT NULL, TITLE TEXT, NOTE TEXT)";
-    db_connect.execute(sql, ())?;
-    println!("DB Connect executed");
+    let db_connect = Connection::open(db_path)?;
+    //println!("\nDEBUG:{}", db_connect.is_autocommit());
+    let table_exists = db_connect.table_exists(None, "notes");
+    // println!("\nDEBUG: If Table Exists = {}",table_exists.unwrap().to_string());
+      match table_exists.as_ref() {
+        Ok(true)=>{
+            println!("\ntable_exists");
+        }
+        Ok(false)=>{
+          if let Err(e) = db_connect.execute(sql, ()){
+            println!("\ncant create: {}",e);
+          }
+        }
+        // _=>{}
+        Err(e)=>
+        {
+            println!("\nHEHE HAHA ERROR {}",e);
+        }
+      }
+        //     true => println!("Table Exists"),
+            // false =>  db_connect.execute(sql, ()),
+        //     _ => println!("some error hehe")
+        // }
+    
+
+    //db_connect.execute(sql, ())?;
+    //println!("{:?}\n{:?}\n{:?}\nDB Connect executed",MAIN_DB,TEMP_DB,SQLITE_ATTACH);
     Ok(())
 }
-fn check_if_note_already_exists() {}
+fn check_if_note_already_exists(db_path:String,note_number:u32) -> Result<()>{
+    // let db_connect = Connection::open(db_path)?;
+    // let sql: &str = format!("SELECT EXISTS(SELECT 1 FROM notes WHERE ID = {}",note_number);
+    // db_connect.execute(&sql,())?;
+    // print!("DB connect Executed. Location: Check if note already exists");
+    // Ok(Result);
+    // Err(_)
+    let db_connect = Connection::open(db_path)?;
+
+    let sql:String = format!("SELECT EXISTS(SELECT 1 FROM notes WHERE ID = {}",note_number);
+    let sql: &str = &sql;
+    db_connect.execute(&sql, ())?;
+    println!("DB Connect and Executed in: Check if note already exists");
+    Ok(())
+
+}
 
 fn get_case_switch_command_number() {}
 
